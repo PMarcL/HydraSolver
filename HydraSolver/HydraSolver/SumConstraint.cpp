9a89162dc6451c3f251284bb26b7bd557b39aa5d
Pierre-Marc Levasseur <pmleva@hotmail.com>
2017-03-06 23:14:13 -0500

    Implemented Trick Algorithm on CPU for sum filtering

--- HydraSolver/HydraSolver/SumConstraint.cpp
+++ HydraSolver/HydraSolver/SumConstraint.cpp
@@ -1,5 +1,8 @@
 #include "SumConstraint.h"
 #include "IntVariable.h"
+#include <algorithm>
+#include <list>
+#include <queue>
 
 using namespace std;
 
@@ -13,7 +16,7 @@ namespace hydra {
 	}
 
 	void SumConstraint::filter() {
-
+		CPUdomainFilteringAlgorithm();
 	}
 
 	void SumConstraint::filterBounds() {
@@ -21,11 +24,103 @@ namespace hydra {
 	}
 
 	void SumConstraint::filterDomains() {
-
+		CPUdomainFilteringAlgorithm();
 	}
 
-	void SumConstraint::CPUdomainFilteringAlgorithm() {
+	struct TrickNode;
 
+	struct TrickArc {
+		TrickArc(int varValue, TrickNode* from, TrickNode* to) : varValue(varValue), from(from), to(to) {}
+		int varValue;
+		TrickNode* from;
+		TrickNode* to;
+	};
+
+	struct TrickNode {
+		explicit TrickNode(int value) : value(value), parent(nullptr) {}
+		int value;
+		TrickArc* parent;
+		vector<TrickArc*> childs;
+	};
+
+	// implementation of Trick algorithm
+	void SumConstraint::CPUdomainFilteringAlgorithm() {
+		list<TrickNode*> nodeQueue;
+		auto initialNode = new TrickNode(0);
+		nodeQueue.push_back(initialNode);
+
+		// building the graph
+		for (auto variable : variables) {
+			auto nodeToProcess = nodeQueue.size();
+			for (size_t i = 0; i < nodeToProcess; i++) {
+				auto currentNode = nodeQueue.front();
+				nodeQueue.pop_front();
+
+				auto iterator = variable->iterator();
+				for (auto v = 0; v < variable->cardinality(); v++) {
+					auto currentValue = iterator->next();
+					auto child = new TrickNode(currentNode->value + currentValue);
+
+					auto arc = new TrickArc(currentValue, currentNode, child);
+					currentNode->childs.push_back(arc);
+					child->parent = arc;
+
+					nodeQueue.push_back(child);
+				}
+				delete iterator;
+			}
+		}
+
+		// nodeQueue here contains all the nodes at the last level
+		// remove any node different than the expected sum
+		for (auto it = nodeQueue.begin(); it != nodeQueue.end();) {
+			if ((*it)->value != sum) {
+				nodeQueue.erase(it++);
+				if (it == nodeQueue.end()) {
+					break;
+				}
+			} else {
+				++it;
+			}
+		}
+
+		// Traversing the graph backward level by level, filtering values along the way
+		for (auto variable = variables.rbegin(); variable != variables.rend(); ++variable) {
+			vector<int> valuesToKeep;
+			auto nodeToProcess = nodeQueue.size();
+
+			for (size_t i = 0; i < nodeToProcess; i++) {
+				auto currentNode = nodeQueue.front();
+				nodeQueue.pop_front();
+
+				valuesToKeep.push_back(currentNode->parent->varValue);
+				nodeQueue.push_back(currentNode->parent->from);
+			}
+
+			auto iterator = (*variable)->iterator();
+			auto originalCardinality = (*variable)->cardinality();
+			for (auto v = 0; v < originalCardinality; v++) {
+				auto currentValue = iterator->next();
+				if (find(valuesToKeep.begin(), valuesToKeep.end(), currentValue) == valuesToKeep.end()) {
+					(*variable)->filterValue(currentValue);
+				}
+			}
+			delete iterator;
+		}
+
+		// Traversing the graph forward to delete all the nodes
+		nodeQueue.erase(nodeQueue.begin(), nodeQueue.end());
+		nodeQueue.push_back(initialNode);
+		while (!nodeQueue.empty()) {
+			auto currentNode = nodeQueue.front();
+			nodeQueue.pop_front();
+
+			for (auto child : currentNode->childs) {
+				nodeQueue.push_back(child->to);
+			}
+
+			delete currentNode;
+		}
 	}
 
 
--- HydraSolver/HydraSolverTest/SumConstraintTest.cpp
+++ HydraSolver/HydraSolverTest/SumConstraintTest.cpp
@@ -14,15 +14,40 @@ public:
 	TEST_METHOD(ShouldFilterDomainsWhenFilterIsCalled_simpleCase) {
 		BitsetIntVariable var1("var1", 1, 2);
 		BitsetIntVariable var2("var2", 1, 3);
-		int sum = 4;
+		auto sum = 4;
 
-		SumConstraint sumConstraint({ &var1, &var2 }, 4);
+		SumConstraint sumConstraint({ &var1, &var2 }, sum);
 
 		sumConstraint.filter();
 
 		// 1 should be filtered from var2, var1 should be unchanged
-		//Assert::IsFalse(var2.containsValue(1));
-		//Assert::AreEqual(2, var1.cardinality());
+		Assert::IsFalse(var2.containsValue(1));
+		Assert::AreEqual(2, var1.cardinality());
+	}
+
+	TEST_METHOD(ShouldFilterDomainsWhenFilterIsCalled_simpleCase2) {
+		BitsetIntVariable var1("var1", 0, 2);
+		BitsetIntVariable var2("var2", 0, 3);
+		BitsetIntVariable var3("var2", 0, 4);
+		BitsetIntVariable var4("var2", 0, 5);
+		auto sum = 14;
+
+		SumConstraint sumConstraint({ &var1, &var2, &var3, &var4 }, sum);
+
+		sumConstraint.filter();
+
+		// should be filter all values except for upper bounds
+		Assert::AreEqual(1, var1.cardinality());
+		Assert::IsTrue(var1.containsValue(2));
+
+		Assert::AreEqual(1, var2.cardinality());
+		Assert::IsTrue(var2.containsValue(3));
+
+		Assert::AreEqual(1, var3.cardinality());
+		Assert::IsTrue(var3.containsValue(4));
+
+		Assert::AreEqual(1, var4.cardinality());
+		Assert::IsTrue(var4.containsValue(5));
 	}
 
 	};
