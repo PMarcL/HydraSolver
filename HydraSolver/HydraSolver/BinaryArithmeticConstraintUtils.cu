de3a42dc40d5c6ab1ab889c41183b7a952dac7da
Pierre-Marc Levasseur <pmleva@hotmail.com>
2017-03-26 18:20:41 -0400

    Fixed projects config

--- HydraSolver/HydraSolver/BinaryArithmeticConstraintUtils.cu
+++ HydraSolver/HydraSolver/BinaryArithmeticConstraintUtils.cu
@@ -4,51 +4,81 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <nvfunctional>
+#include <stdio.h>
 
 using namespace std;
 
 namespace hydra {
 
 	nvstd::function<bool(int, int)> getOperation(Operator op, RelationalOperator relop, int rhs) {
-		nvstd::function<int(int, int)> lhsResult;
-		switch (op) {
-		case PLUS:
-			lhsResult = [] __device__(int v1, int v2) { return v1 + v2; };
-			break;
-		case MINUS:
-			lhsResult = []  __device__(int v1, int v2) { return v1 - v2; };
-			break;
-		case MULTIPLIES:
-			lhsResult = []  __device__(int v1, int v2) { return v1 * v2; };
-			break;
-		case DIVIDES:
-			lhsResult = []  __device__(int v1, int v2) { return v1 / v2; };
-			break;
-		}
-
-		nvstd::function<bool(int, int)> operation;
 		switch (relop) {
 		case EQ:
-			operation = [lhsResult, rhs]  __device__(int v1, int v2) { return lhsResult(v1, v2) == rhs; };
-			break;
-		case NEQ:
-			operation = [lhsResult, rhs]  __device__(int v1, int v2) { return lhsResult(v1, v2) != rhs; };
-			break;
-		case GEQ:
-			operation = [lhsResult, rhs]  __device__(int v1, int v2) { return lhsResult(v1, v2) >= rhs; };
-			break;
-		case GT:
-			operation = [lhsResult, rhs]  __device__(int v1, int v2) { return lhsResult(v1, v2) > rhs; };
-			break;
-		case LEQ:
-			operation = [lhsResult, rhs]  __device__(int v1, int v2) { return lhsResult(v1, v2) <= rhs; };
-			break;
-		case LT:
-			operation = [lhsResult, rhs]  __device__(int v1, int v2) { return lhsResult(v1, v2) < rhs; };
-			break;
+			switch (op) {
+			case PLUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 + v2 == rhs; };
+			case MINUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 - v2 == rhs; };
+			case MULTIPLIES:
+				return[rhs]  __device__(int v1, int v2) { return v1 * v2 == rhs; };
+			case DIVIDES:
+				return[rhs]  __device__(int v1, int v2) { return v1 / v2 == rhs; };
+			}
+		case NEQ:													   
+			switch (op) {
+			case PLUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 + v2 != rhs; };
+			case MINUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 - v2 != rhs; };
+			case MULTIPLIES:
+				return[rhs]  __device__(int v1, int v2) { return v1 * v2 != rhs; };
+			case DIVIDES:
+				return[rhs]  __device__(int v1, int v2) { return v1 / v2 != rhs; };
+			}
+		case GEQ:													   
+			switch (op) {
+			case PLUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 + v2 >= rhs; };
+			case MINUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 - v2 >= rhs; };
+			case MULTIPLIES:
+				return[rhs]  __device__(int v1, int v2) { return v1 * v2 >= rhs; };
+			case DIVIDES:
+				return[rhs]  __device__(int v1, int v2) { return v1 / v2 >= rhs; };
+			}
+		case GT:													   
+			switch (op) {
+			case PLUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 + v2 > rhs; };
+			case MINUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 - v2 > rhs; };
+			case MULTIPLIES:
+				return[rhs]  __device__(int v1, int v2) { return v1 * v2 > rhs; };
+			case DIVIDES:
+				return[rhs]  __device__(int v1, int v2) { return v1 / v2 > rhs; };
+			}
+		case LEQ:													   
+			switch (op) {
+			case PLUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 + v2 <= rhs; };
+			case MINUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 - v2 <= rhs; };
+			case MULTIPLIES:
+				return[rhs]  __device__(int v1, int v2) { return v1 * v2 <= rhs; };
+			case DIVIDES:
+				return[rhs]  __device__(int v1, int v2) { return v1 / v2 <= rhs; };
+			}
+		case LT:													   
+			switch (op) {
+			case PLUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 + v2 < rhs; };
+			case MINUS:
+				return[rhs]  __device__(int v1, int v2) { return v1 - v2 < rhs; };
+			case MULTIPLIES:
+				return[rhs]  __device__(int v1, int v2) { return v1 * v2 < rhs; };
+			case DIVIDES:
+				return[rhs]  __device__(int v1, int v2) { return v1 / v2 < rhs; };
+			}
 		}
-
-		return operation;
 	}
 
 	bool filterVariableBounds(BitsetIntVariable* varToFilter, BitsetIntVariable* otherVar, nvstd::function<bool(int, int)> operation);
@@ -72,32 +102,36 @@ namespace hydra {
 		return filteredVariables;
 	}
 
-	__constant__ int lb;
-	__constant__ int ub;
-	__constant__ int originalLowerBound;
-
 	template <typename Operation>
-	__global__ void filterBoundDevice(Operation op, uint8_t *result) {
-		int value = originalLowerBound + threadIdx.x;
-		result[threadIdx.x] = op(value, lb) || op(value, ub);
+	__global__ void filterBoundDevice(Operation op, int *lb, int *ub, int *originalLowerBound, uint8_t *result) {
+		int value = *originalLowerBound + threadIdx.x;
+		printf("Thread id : %d\n", threadIdx.x);
+		printf("Value : %d\n", value);
+		result[threadIdx.x] = op(value, *lb) || op(value, *ub);
 	}
 
 	bool filterVariableBounds(BitsetIntVariable* varToFilter, BitsetIntVariable* otherVar, nvstd::function<bool(int, int)> operation) {
-		auto lowerbound = otherVar->getUpperBound();
-		cudaMemcpyToSymbol("lb", &lowerbound, sizeof(int));
+		auto lb = otherVar->getLowerBound();
+		int *device_lb;
+		cudaMalloc((void**)&device_lb, sizeof(int));
+		cudaMemcpy(device_lb, &lb, sizeof(int), cudaMemcpyHostToDevice);
 
-		auto upperbound = otherVar->getUpperBound();
-		cudaMemcpyToSymbol("ub", &upperbound, sizeof(int));
+		auto ub = otherVar->getUpperBound();
+		int *device_ub;
+		cudaMalloc((void**)&device_ub, sizeof(int));
+		cudaMemcpy(device_ub, &ub, sizeof(int), cudaMemcpyHostToDevice);
 
 		auto originalLowerBound = varToFilter->getOriginalLowerBound();
-		cudaMemcpyToSymbol("originalLowerBound", &originalLowerBound, sizeof(int));
+		int *device_originallb;
+		cudaMalloc((void**)&device_originallb, sizeof(int));
+		cudaMemcpy(device_originallb, &originalLowerBound, sizeof(int), cudaMemcpyHostToDevice);
 
 		uint8_t *bitset_device, *bitset_host;
 		auto size = varToFilter->getOriginalSize();
 		bitset_host = (uint8_t *)malloc(size);
 		cudaMalloc((void **)&bitset_device, size);
 
-		filterBoundDevice<<<1, size>>>(operation, bitset_device);
+		filterBoundDevice<<<1, size>>>(operation, device_lb, device_ub, device_originallb, bitset_device);
 
 		cudaMemcpy(bitset_host, bitset_device, size, cudaMemcpyDeviceToHost);
 
