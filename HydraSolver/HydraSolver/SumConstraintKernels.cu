5e7f7d578060d37bcad1d6c1dccf29b391546182
Jean-François Tremblay <Jean-Franois Tremblay>
2017-04-06 15:03:10 -0400

    Fixed 1024 domain limit problem. Created memory corruption problem. Need code revision.

--- HydraSolver/HydraSolver/SumConstraintKernels.cu
+++ HydraSolver/HydraSolver/SumConstraintKernels.cu
@@ -3,9 +3,9 @@
 #include "device_launch_parameters.h"
 
 __global__ void filterVariableKernel(int sum, int lowerBoundSum, int upperBoundSum, int originalLowerBound, uint8_t* pBitset) {
-	int value = threadIdx.x + originalLowerBound;
+	int value = threadIdx.x + 1024 * threadIdx.y + originalLowerBound;
 	lowerBoundSum += value;
 	upperBoundSum += value;
 	auto hasSupport = sum >= lowerBoundSum && sum <= upperBoundSum;
-	pBitset[threadIdx.x] = uint8_t(hasSupport);
+	pBitset[1024 * threadIdx.y + threadIdx.x] = uint8_t(hasSupport);
 }
--- HydraSolver/HydraSolver/SumConstraintUtils.cu
+++ HydraSolver/HydraSolver/SumConstraintUtils.cu
@@ -1,13 +1,24 @@
 #include "SumConstraintUtils.cuh"
 #include "SumConstraintKernels.cuh"
 #include "cuda_runtime.h"
+#include "HydraException.h"
+#include <iostream>
 
 void launchFilteringKernels(int nKernel, int sum, int lowerBoundSum, int upperBoundSum, int originalLowerBound, std::vector<uint8_t>* bitSetPtr) {
 	uint8_t* deviceBitSetPtr;
-	cudaMalloc((void**)&deviceBitSetPtr, bitSetPtr->size() * sizeof(uint8_t));
-
-	filterVariableKernel << < 1, nKernel >> > (sum, lowerBoundSum, upperBoundSum, originalLowerBound, deviceBitSetPtr);
-
-	cudaMemcpy(bitSetPtr->data(), deviceBitSetPtr, bitSetPtr->size() * sizeof(uint8_t), cudaMemcpyDeviceToHost);
-	cudaFree(deviceBitSetPtr);
+	while (nKernel % 1024 != 0) nKernel++;
+	auto error = cudaMalloc((void**)&deviceBitSetPtr, nKernel * sizeof(uint8_t));
+	if (error != cudaSuccess) {
+		throw hydra::HydraException("Cuda memory allocation error.");
+	}
+	auto nBlocks = nKernel / 1024;
+	filterVariableKernel << < nBlocks, 1024 >> > (sum, lowerBoundSum, upperBoundSum, originalLowerBound, deviceBitSetPtr);
+	error = cudaMemcpy(bitSetPtr->data(), deviceBitSetPtr, bitSetPtr->size() * sizeof(uint8_t), cudaMemcpyDeviceToHost);
+	if (error != cudaSuccess) {
+		throw hydra::HydraException("Cuda memory copy error");
+	}
+	error = cudaFree(deviceBitSetPtr);
+	if (error != cudaSuccess) {
+		throw hydra::HydraException("Cuda free memory error");
+	}
 }
